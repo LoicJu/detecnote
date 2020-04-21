23a7076d245b8989e701b42de8324ae131cf36e1
loic.jurasz <loic.jurasz@he-arc.ch>
2020-04-21 19:22:09 +0200

    finishing to translate the notes

--- Main.py
+++ Main.py
@@ -12,12 +12,11 @@ import cv2
 import glob
 import sys
 import numpy as np
-'''
-from skimage import data, color, img_as_ubyte
-from skimage.feature import canny
-from skimage.transform import hough_ellipse
-from skimage.draw import ellipse_perimeter
-'''
+
+show_image = True
+
+def takeX(elem):
+    return elem[0][0][0]
 
 def analyse(img, nameimg):
     # we resize because the image was too big
@@ -66,11 +65,10 @@ def analyse(img, nameimg):
         # test if it's not part of the title (if it's just above the top horizontal line, it' ok) and if the line begin before or at the same place as an "classic" horizontal line, it's a long line so we don't take it
         if cnts_hor[-1][0][0][1]-20<c[0][0][1] and cnts_ver[-1][0][0][0]<c[0][0][0]:
             cnts_hor_short_final.append(c)
-            cv2.drawContours(result_intermediate, [c], -1, (0,0,255), 2)
     cv2.drawContours(result_intermediate,cnts_hor_short_final, -1, (0,255,255), 2)
     
     # now we search verticale lines that are from notes like "noir" or "croche"
-    # cnts_ver_short_final will have all the little horizontal lines that interested us
+    # cnts_ver_short_final will have all the little vertical lines that interested us
     cnts_ver_short_final=[]
     vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1,10)) # 1,10 to take short lines
     detect_vertical = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, vertical_kernel, iterations=2)
@@ -79,12 +77,29 @@ def analyse(img, nameimg):
     for c in cnts_ver_short:
         # test if the length of the vertical line isn't too big (as classic vertical bar), then, test if it's bewteen the base lines
         if abs(c[0][0][1]-c[1][0][1])< abs(cnts_ver[-1][0][0][1]-cnts_ver[-1][0][1][1]) and cnts_ver[-1][0][0][0]<c[0][0][0] and cnts_hor[-1][0][0][1]-20<c[0][0][1]:
-            cv2.drawContours(result_intermediate, [c], -1, (0,255,255), 2)
+            cnts_ver_short_final.append(c)
+    cv2.drawContours(result_intermediate, cnts_ver_short_final, -1, (0,255,255), 2)
+
     # draw all vertical lines and horizontal lines in white
     for c in cnts_hor:
         cv2.drawContours(result, [c], -1, (255,255,255), 2)
     for c in cnts_ver:
         cv2.drawContours(result, [c], -1, (255,255,255), 2)
+    for c in cnts_hor_short_final:
+        cv2.drawContours(result, [c], -1, (255,255,255), 2)
+    for c in cnts_ver_short_final:
+        cv2.drawContours(result, [c], -1, (255,255,255), 2)
+
+    # detect the highest horizontal line
+    highest_hor_line = cnts_hor[-1][0][0][1]
+    for c in cnts_hor_short_final:
+        if c[0][0][1]<cnts_hor[-1][0][0][1]:
+            highest_hor_line = c[0][0][1]
+    # detect the lowest horizontal line
+    lowest_hor_line = cnts_hor[0][0][0][1]
+    for c in cnts_hor_short_final:
+        if c[0][0][1]>cnts_hor[0][0][0][1]:
+            lowest_hor_line = c[0][0][1]
     # transform image to detect contours
     result_gray = cv2.cvtColor(result, cv2.COLOR_RGB2GRAY)
     _, result_binary = cv2.threshold(result_gray, 225, 255, cv2.THRESH_BINARY_INV)
@@ -94,26 +109,161 @@ def analyse(img, nameimg):
     cv2.drawContours(result, contours, -1, (255, 255, 255), thickness=cv2.FILLED)
     contours_finals = []
     for c in contours:
-        # test if contours are between our lines
-        if not(c[0][0][1] > cnts_hor[0][0][0][1] or c[0][0][1] < cnts_hor[-1][0][0][1] or c[0][0][0] > cnts_ver[0][0][0][0] or c[0][0][0] < cnts_ver[-1][0][0][0]):
+        # test if contours are between our lines (first if it's not below, second if it's not higher, third not to the right, fourth to te left)
+        if not((c[0][0][1] > lowest_hor_line+20) or (c[0][0][1] < highest_hor_line-20) or (c[0][0][0] > cnts_ver[0][0][0][0]) or (c[0][0][0] < cnts_ver[-1][0][0][0])):
             # we add the contours that are between our lines in contours_final
             contours_finals.append(c)
     # we draw contours final
-    result = cv2.drawContours(result, contours_finals, -1, (0, 255, 0), thickness=cv2.FILLED)
+    cv2.drawContours(result, contours_finals, -1, (0, 0, 0), thickness=cv2.FILLED)
 
-    cv2.imshow('lines',result_intermediate)
-    cv2.imshow('detected contours',result)
-    cv2.imshow("image de depart", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # we dilate the image result and erode it, we can get off of some points that were there and we have entire notes
+    result_gray = cv2.cvtColor(result, cv2.COLOR_RGB2GRAY)
+    _, result_binary = cv2.threshold(result_gray, 225, 255, cv2.THRESH_BINARY_INV)
+    kernel = np.ones((8,8),np.uint8)
+    result_binary = cv2.dilate(result_binary, kernel)
+    kernel = np.ones((11,11),np.uint8)
+    result_binary = cv2.erode(result_binary, kernel)
 
-    # write in file what are the notes TODO
+    # now we only have the entire notes
+    contours_notes, hierarchy = cv2.findContours(result_binary, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    # order contours_notes from right to left
+    contours_notes.sort(key=takeX, reverse=True)
+    # we can know which note is which with the lines, we know that the long lines are grouped by 5
+    lines = []
+    for i in range(0, len(cnts_hor)-1, +5):
+        lines.append("\n")
+        # we'll add little lines in this list
+        cnts_hor_littles = cnts_hor.copy()
+        # each note
+        for c2 in contours_notes:
+            # need to check for little lines, above or under the 5 principal lines
+            nbLines = 5
+            aboveLine = cnts_hor[i+4][0][0][1]
+            line_to_add_above = cnts_hor[i+4]
+            underLine = cnts_hor[i][0][0][1]
+            line_to_add_below = cnts_hor[i]
+            insert_above = False
+            insert_under = False
+            # under
+            for clittles in cnts_hor_short:
+                if clittles[0][0][1] > underLine and clittles[0][0][1] < underLine+20:
+                    underLine = clittles[0][0][1]
+                    line_to_add_below = clittles
+                    insert_under = True
+            if insert_under:
+                nbLines += 1
+                cnts_hor_littles.insert(i,line_to_add_below)
+             # above
+            for clittles in cnts_hor_short:
+                if clittles[0][0][1] < aboveLine and clittles[0][0][1] > aboveLine-20:
+                    aboveLine = clittles[0][0][1]
+                    line_to_add_above = clittles
+                    insert_above = True
+            if insert_above:
+                nbLines += 1
+                cnts_hor_littles.insert(i+5,line_to_add_above)
+            # if the note is between the 5 lines + and - 15 is to take the notes that are just above or under the line
+            if c2[0][0][1]<underLine+15 and c2[0][0][1]>aboveLine-15:
+                # each point in the note
+                above = 0
+                # if it's on the line
+                on_line = False   
+                # we parcour the lines 
+                for j in range(0, nbLines):
+                    above_the_line = False
+                    under_the_line = False 
+                    # we parcour all the points in the contours to compare to the line in order to know if it's above and under the line (if it's on the line)       
+                    for c3 in c2:
+                        if c3[0][1]>cnts_hor_littles[j+i][0][0][1]:
+                            under_the_line = True
+                        if c3[0][1]<cnts_hor_littles[j+i][0][0][1]:
+                            above_the_line = True
+                        if under_the_line and above_the_line:
+                            on_line = True
+                    if c2[0][0][1]<cnts_hor_littles[j+i][0][0][1]:
+                       above += 1 
+                # write in lines what notes it is
+                # if insert under OR under and above
+                if insert_under:
+                    if on_line:
+                        if above == 1:
+                            lines.append('Do')
+                        if above == 2:
+                            lines.append('Mi')
+                        if above == 3:
+                            lines.append('Sol')
+                        if above == 4:
+                            lines.append('Si')
+                        if above == 5:
+                            lines.append('Re')
+                        if above == 6:
+                            lines.append('Fa')
+                        if above == 7:
+                            lines.append('La')
+                    else:                      
+                        if above == 1:
+                            lines.append('Re')
+                        if above == 2:
+                            lines.append('Fa')
+                        if above == 3:
+                            lines.append('La')
+                        if above == 4:
+                            lines.append('Do')
+                        if above == 5:
+                            lines.append('Mi')
+                        if above == 6:
+                            lines.append('Sol')
+                        if above == 7:
+                            lines.append('Si')
+                else:
+                    if on_line:
+                        if above == 0:
+                            lines.append('Mi')
+                        if above == 1:
+                            lines.append('Sol')
+                        if above == 2:
+                            lines.append('Si')
+                        if above == 3:
+                            lines.append('Re')
+                        if above == 4:
+                            lines.append('Fa')
+                        if above == 5:
+                            lines.append('La')
+                    else:                      
+                        if above == 0:
+                            lines.append('Re')
+                        if above == 1:
+                            lines.append('Fa')
+                        if above == 2:
+                            lines.append('La')
+                        if above == 3:
+                            lines.append('Do')
+                        if above == 4:
+                            lines.append('Mi')
+                        if above == 5:
+                            lines.append('Sol')
+
+            if insert_above:
+                cnts_hor_littles.pop(i+5)
+            if insert_under:
+                cnts_hor_littles.pop(i-1)
+    lines.reverse()
+    # write in file what are the notes
     f= open(nameimg + ".txt","w+")
-    f.write("hello")
+    for n in lines:
+        f.write(n + " ")
     f.close()
 
+    if show_image:
+        cv2.imshow("image de depart", img)
+        cv2.imshow('contours détéctés',result)
+        cv2.imshow('contour binaire',result_binary)
+        cv2.imshow('Detection de lines',result_intermediate)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
 if __name__ == "__main__":
-    filenames = [img for img in glob.glob("Images/partition1_wbc.png")]
+    filenames = [img for img in glob.glob("Images/*.png")]
     for f1 in filenames:
         img = cv2.imread(f1)
         analyse(img, f1)
--- README.md
+++ README.md
@@ -6,3 +6,5 @@ He-Arc 2020
 François Bouthillier de Beaumont - Roxane Carraux - Loïc Jurasz
 
 Programme utilisant OpenCV et développé en langage python permettant de détecter des notes dans une partition de musique
+
+Le programme pour faire les partitions est MuseScore3.
